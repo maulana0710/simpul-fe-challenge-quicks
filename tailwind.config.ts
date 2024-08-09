faf02160a809266583bb560b77e5cff0c56d46f9
Vicky Maulana <vickymaulana0710@gmail.com>
2024-08-09 13:13:57 +0700

    fix: setup for development

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,43 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        // Base colors
+        primary: "#2F80ED",
+        secondary: "#4F4F4F",
+        tertiary: "#828282",
+        quaternary: "#E0E0E0",
+
+        // Indicators
+        indicator: {
+          primary: "#F8B76B",
+          secondary: "#8785FF",
+          tertiary: "#EB5757",
+          quaternary: "#F2C94C",
+        },
+
+        // Chat colors
+        chat: {
+          primary: "#FCEED3",
+          primarySec: "#E5A443",
+          secondary: "#EEDCFF",
+          secondarySec: "#9B51E0",
+          tertiary: "#D2F2EA",
+          tertiarySec: "#43B78D",
+        },
+
+        // Stiker colors
+        sticker: {
+          primary: "#E9F3FF",
+          secondary: "#FDCFA4",
+          tertiary: "#F9E9C3",
+          quaternary: "#AFEBDB",
+          quinary: "#CBF1C2",
+          senary: "#CFCEF9",
+          septenary: "#F9E0FD",
+        },
       },
+      backgroundImage: {},
     },
   },
   plugins: [],
