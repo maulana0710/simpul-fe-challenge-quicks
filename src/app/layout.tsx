d0bb1bd567b4983b21485246b12a77b1fe37fc4f
Vicky Maulana <vickymaulana0710@gmail.com>
2024-08-11 10:29:03 +0700

    feat: add LoadingIndicator to handle fully load from response API, adding icon search-black and add font family 'Lato' as default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from 'next/head'; // Import Head from next/head
+import Head from "next/head"; // Import Head from next/head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
